243e08416cccc5b46020c1149058a85d562d5247
AjayKhati <ajaysingh0001123@gmail.com>
2025-06-22 13:52:23 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import Home from './components/Home'
-// import Opening from './components/Opening'
+import React from 'react';
+import Home from './components/Home';
 
 function App() {
-
- const [isSplashVisible, setSplashVisible] = useState(true);
-
- useEffect(()=>{
-  const timer = setTimeout(()=>{
-    setSplashVisible(false);
-  },4000)
-
-  return ()=> clearTimeout(timer);
- },[])
-
-
   return (
     <>
-    {isSplashVisible && <Opening/>}
-    <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
